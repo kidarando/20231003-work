173ac66f1c5b6fce53ba1c8d422ff942d19067e5
kidarando <kd1338465@st.kobedenshi.ac.jp>
2023-10-03 11:45:06 +0900

    提出

--- ex2/work.py
+++ ex2/work.py
@@ -1,8 +1,30 @@
 from math import ceil
 
 def calc_account(m):
-    # 実装は入れていません、自分で入れてください
-    pass
+    # 初乗り距離と運賃
+    initial_distance = 1700
+    initial_fare = 610
+
+    # それ以降の増加距離と増加運賃
+    increment_distance = 315
+    increment_fare = 80
+
+    # 距離が0m以下の場合
+    if m <= 0:
+        return None
+
+    # 初乗り距離までの場合
+    if m <= initial_distance:
+        return initial_fare
+
+    # 初乗り距離を超えた場合
+    # 初乗り運賃に加えて、315m毎に80円増える
+    excess_distance = m - initial_distance
+    increments = ceil(excess_distance / increment_distance)
+    total_fare = initial_fare + increments * increment_fare
+
+    return total_fare
+
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
